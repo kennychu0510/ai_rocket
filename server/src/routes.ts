d584912d4647741a33e180000f4016044ea8e099
Henry Lau <henrylauforwork@gmail.com>
2022-06-07 18:49:19 +0800

    test

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,4 +6,4 @@ export const routes = express.Router();
 // star map routes
 routes.get('/easy-mode', mapController.get);
 routes.post('/map', mapController.create);
-// routes.post('/test', starMapController.test);
+
